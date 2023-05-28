f0f3c0332bfa12f4066144d560d01766741453d8
Glenn <gcrosby15@gmail.com>
2023-05-28 22:38:42 +0700

    add POST form to view to add to-do item

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
+# from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page 
@@ -14,24 +14,15 @@ class HomePageTest(TestCase):
 
 
 	def test_home_page_returns_correct_html(self):
-		# request = HttpRequest() 
-		# response = home_page(request)
-
-
-
-		# html = response.content.decode('utf8')
-
-		# expected_html = render_to_string('home.html')
-
-		# self.assertTrue(html.startswith('<!DOCTYPE html>'))
-		# self.assertIn("<title>To-Do lists</title>", html)
-		# self.assertTrue(html.strip().endswith('</html>'))
-
 		response = self.client.get('/')
 		self.assertTrue(response.status_code, 200)
 		self.assertTemplateUsed(response, 'home.html')
 
 
+	def test_can_save_a_POST_request(self):
+		response = self.client.post('/', data={'item_text': "A new list item"})
+		self.assertIn("A new list item", response.content.decode())
+		# self.assertTemplateUsed(response, 'home.html')
 
 # class SmokeTest(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -5,5 +5,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-	return render(request, 'home.html')
+	return render(
+		request, 
+		'home.html',
+		{
+			'new_item_text': request.POST.get('item_text', '')
+		}
+	)
 	# return HttpResponse("<html><title>To-Do lists</title></html>")
