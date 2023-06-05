06893155628dc221b637cd00a90e5e791d6fb51f
Glenn <gcrosby15@gmail.com>
2023-06-05 20:48:59 +0700

    enforce model validation in list view

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -68,22 +68,6 @@ class NewListTest(TestCase):
 		self.assertEqual(Item.objects.count(), 0)
 
 
-
-
-# class SmokeTest(TestCase):
-
-# 	def test_bad_maths(self):
-# 		self.assertEqual(1 + 1, 3)
-
-
-# class NewItemTest(TestCase):
-
-
-
-
-
-
-
 class ListViewTest(TestCase):
 	def test_uses_list_template(self):
 		list_ = List.objects.create()
@@ -141,4 +125,33 @@ class ListViewTest(TestCase):
 		)
 
 		self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+	def test_validation_errors_end_up_on_lists_page(self):
+		list_ = List.objects.create()
+		response = self.client.post(
+			f'/lists/{list_.id}/',
+			data={'item_text': ''}
+		)
+
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'list.html')
+		expected_error = escape("You can't have an empty list item")
+		self.assertContains(response, expected_error)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- lists/views.py
+++ lists/views.py
@@ -16,25 +16,32 @@ def home_page(request):
 
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
+	error = None 
 
 	if request.method == 'POST':
-		item = Item.objects.create(text=request.POST['item_text'], list=list_)
-		return redirect(f'/lists/{list_.id}/')
+		try:
+			item = Item.objects.create(text=request.POST['item_text'], list=list_)
+			item.full_clean()
+			item.save()
+			return redirect(f'/lists/{list_.id}/')
+
+		except ValidationError:
+			item.delete()
+			error = "You can't have an empty list item"
 
 	# items = Item.objects.filter(list=list_)
 
-	return render(request, 'list.html', {'list': list_})
+	return render(request, 'list.html', {'list': list_, 'error': error})
  
 
 def new_list(request):
 
 	list_ = List.objects.create()
-
 	item = Item.objects.create(text=request.POST['item_text'], list=list_)
 
 	try:
 		item.full_clean()
-
+		item.save()
 	except ValidationError:
 		list_.delete()
 		error = "You can't have an empty list item"
