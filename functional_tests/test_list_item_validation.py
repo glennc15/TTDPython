98413daac9056612fc72e06304bda207b9c22e02
Glenn <gcrosby15@gmail.com>
2023-06-11 12:59:26 +0700

    add function test for form duplication error message disapears if user start changing input, test is failing

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -59,43 +59,42 @@ class ItemValidationTest(FunctionalTest):
 
 		# She sees a helpful error message
 		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has-error').text,
+			self.get_error_element().text,
 			"You've already got this in your list"
 		))
 
 
-	# def test_cannot_add_empty_list_items(self):
-	# 	# Edith goes to the home page and accidentally tries to submit
-	# 	# an empty list item. She hits Enter on the empty input box
-	# 	self.browser.get(self.live_server_url)
-	# 	self.get_item_input_box().send_keys(Keys.ENTER)
+	def test_error_messages_are_cleared_on_input(self):
+		# Edith starts a list and causes a validation error:
+		self.browser.get(self.live_server_url)
+		self.get_item_input_box().send_keys('Banter too thick')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Banter too thick')
+		self.get_item_input_box().send_keys('Banter too thick')
+		self.get_item_input_box().send_keys(Keys.ENTER)
 
-	# 	# The home page refreshes, and there is an error message saying
-	# 	# that list items cannot be blank
-	# 	self.wait_for(lambda: self.assertEqual(
-	# 		self.browser.find_element_by_css_selector('.has-error').text,
-	# 		"You can't have an empty list item"
-	# 	))
+		self.wait_for(lambda: self.assertTrue(  
+			self.get_error_element().is_displayed()  
+		))
 
-	# 	# She tries again with some text for the item, which now works
-	# 	self.get_item_input_box().send_keys('Buy milk')
-	# 	self.get_item_input_box().send_keys(Keys.ENTER)
-	# 	self.wait_for_row_in_list_table('1: Buy milk')
+		# She starts typing in the input box to clear the error
+		self.get_item_input_box().send_keys('a')
 
-	# 	# Perversely, she now decides to submit a second blank list item
-	# 	self.get_item_input_box().send_keys(Keys.ENTER)
+		# She is pleased to see that the error message disappears
+		self.wait_for(lambda: self.assertFalse(
+			self.get_error_element().is_displayed()  
+		))
 
-	# 	# She receives a similar warning on the list page
-	# 	self.wait_for(lambda: self.assertEqual(
-	# 		self.browser.find_element_by_css_selector('.has-error').text,
-	# 		"You can't have an empty list item"
-	# 	))
+	# ***************************************************************
+	# Helper Methods:
 
-	# 	# And she can correct it by filling some text in
-	# 	self.get_item_input_box().send_keys('Make tea')
-	# 	self.get_item_input_box().send_keys(Keys.ENTER)
-	# 	self.wait_for_row_in_list_table('1: Buy milk')
-	# 	self.wait_for_row_in_list_table('2: Make tea')
+	def get_error_element(self):
+		return self.browser.find_element_by_css_selector('.has-error')
+
+
+	# ***************************************************************
+
+
 
 
 
